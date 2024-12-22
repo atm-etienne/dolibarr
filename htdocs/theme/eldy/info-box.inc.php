214b7ba0d8be86a2e7c805b9c5766d0266b79d9f
Etienne BENDER <etienne.bender@atm-consulting.fr>
2024-12-22 17:27:51 +0100

    Style: replace inappropriate JS with CSS

--- htdocs/theme/eldy/info-box.inc.php
+++ htdocs/theme/eldy/info-box.inc.php
@@ -346,36 +346,32 @@ if (GETPOSTISSET('THEME_SATURATE_RATIO')) {
 	<?php } ?>
 }
 
-.nonature-back {
-	background-color: #EEE;
-	padding: 2px;
-	margin: 2px;
+.nature label {
+	background: #EEE;
+	color: #000;
+	padding: 5px 10px;
 	border-radius: 3px;
+	cursor: pointer;
 }
-.prospect-back {
-	background-color: #a7c5b0 !important;
-	color: #FFF !important;
-	padding: 2px;
-	margin: 2px;
-	border-radius: 3px;
+.nature input[type="checkbox"] {
+	display: none;
 }
-.customer-back {
-	background-color: #55955d !important;
-	color: #FFF !important;
-	padding: 2px;
-	margin: 2px;
-	border-radius: 3px;
+
+.nature input:checked + label {
+	color: #fff;
 }
-.vendor-back {
-	background-color: #599caf !important;
-	color: #FFF !important;
-	padding: 2px;
-	margin: 2px;
-	border-radius: 3px;
+.nature input#prospect-input:checked + label {
+	background: #a7c5b0;
+}
+.nature input#customer-input:checked + label {
+	background: #65953d;
+}
+.nature input#supplier-input:checked + label {
+	background: #599caf;
 }
 .user-back {
-	background-color: #79633f !important;
-	color: #FFF !important;
+	background-color: #79633f;
+	color: #FFF;
 	padding: 2px;
 	margin: 2px;
 	border-radius: 3px;
